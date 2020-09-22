44b2f70f51d134f6854723b82a1945f4357e56e4
jackson211 <jackson21182@gmail.com>
2020-09-22 15:00:24 +0800

    rm tabs

--- visualization.py
+++ visualization.py
@@ -9,22 +9,13 @@ import holoviews as hv
 from holoviews import opts
 from holoviews.operation.datashader import regrid, shade
 from bokeh.io import output_file, save, show
+from bokeh.models import HoverTool
 from bokeh.resources import CDN
 from bokeh.embed import file_html
 from bokeh.plotting import figure
 
 
 nodata = 1
-
-# css = '''
-# div.title-txt {
-#     font-family: 'Helvetica', sans-serif,
-#     font-weight: bold,
-#     color: white
-# }
-# '''
-
-# pn.extension(raw_css=[css])
 
 
 css = """
@@ -65,47 +56,57 @@ def combine_bands(band):
 def main(path):
     band = read_tiff(path)
 
+    # tooltips = [
+    #     ('Longitude', '@x'),
+    #     ('Latitude', '@y'),
+    # ]
+
+    # hover = HoverTool(tooltips=tooltips)
     # Combing images
     layout = regrid(combine_bands(
         band) + one_band(band[0]) + one_band(band[1])+one_band(band[2])).redim(x='Longitude', y='Latitude')
     layout.opts(
-        opts.RGB(width=600, height=468, framewise=True)).cols(2)
+        opts.RGB(width=600, height=468, framewise=True,  tools=['hover', 'tap'])).cols(2)
 
     # Figure
     p1 = figure(width=300, height=300)
     p1.line([1, 2, 3], [1, 2, 3])
 
-    p2 = pn.panel("""
-    <div class="special_table"></div>
+    # p2 = pn.panel("""
+    # <div class="special_table"></div>
 
-    | Syntax | Description |
-    | ----------- | ----------- |
-    | Header | Title |
-    | Paragraph | Text |
+    # | Syntax | Description |
+    # | ----------- | ----------- |
+    # | Header | Title |
+    # | Paragraph | Text |
 
-    """)
+    # """)
 
-    # Build panel
-    tabs = pn.Tabs(("Raster Image Channels", layout))
-    tabs.extend([('Some', p1),
-                 ('Random', p2),
-                 ('Shit', pn.widgets.ColorPicker())
-                 ])
+    cols = pn.Column()
+
+    title = pn.panel("""
+                                 <div class="title-txt"></div>
+                                 # Remote Sensing Image Viewer
+                     """)
+    cols.append(title)
+    cols.append(layout)
+    cols.append(p1)
 
     # Grid layout
-    gspec = pn.GridSpec(sizing_mode='stretch_width',
-                        max_width=600, height=100)
+    # gspec = pn.GridSpec(sizing_mode='stretch_width', max_width=600)
 
-    gspec[0, 0] = pn.Spacer(margin=0)
-    gspec[0, 1:6] = pn.panel(""" 
-                                <div class="title-txt"></div>
-                                # Remote Sensing Image Viewer
-                            """
-                             )
-    gspec[0, 6] = pn.Spacer(margin=0)
-    gspec[1, 1:6] = tabs
+    # gspec[0, 0] = pn.Spacer(margin=0)
+    # gspec[0, 1:6] = pn.panel("""
+    #                             <div class="title-txt"></div>
+    #                             # Remote Sensing Image Viewer
+    #                         """
+    #                          )
+    # gspec[0, 6] = pn.Spacer(margin=0)
+    # gspec[1, 1:6] = layout
+    # gspec[3:, 1:6] = p1
+
     # print(gspec.grid)
-    gspec.show(title='Remote Sensing')
+    cols.show(title='Remote Sensing')
 
 
 if __name__ == "__main__":
